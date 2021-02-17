b793757d3aa0517959268ef84a812af9c44915ce
ayah1103 <73839197+ayah1103@users.noreply.github.com>
2021-02-17 02:38:10 +0100

    add php config, make pin entity timestampable , listing on infex by createdAt desc

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -15,7 +15,7 @@ class PinsController extends AbstractController
      */
     public function index(PinRepository $pinRepository)
     {
-        $pins= $pinRepository->findAll();
+        $pins= $pinRepository->findBy([],['createdAt' => 'DESC']);
         return $this->render('pins/index.html.twig', compact('pins'));
     }
 
--- src/Entity/Pin.php
+++ src/Entity/Pin.php
@@ -8,6 +8,7 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * @ORM\Entity(repositoryClass=PinRepository::class)
  * @ORM\Table(name="pins")
+ * @ORM\HasLifecycleCallbacks
  * */
 class Pin
 {
@@ -28,6 +29,16 @@ class Pin
      * @ORM\Column(type="text")
      */
     private $description="";
+
+    /**
+     * @ORM\Column(type="datetime", options={"default":"CURRENT_TIMESTAMP"})
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime", options={"default":"CURRENT_TIMESTAMP"})
+     */
+    private $updatedAt;
 
     public function getId(): ?int
     {
@@ -54,4 +65,36 @@ class Pin
         return $this;
     }
 
+    public function getCreatedAt() : ?\DateTimeInterface 
+    {
+        return $this->createdAt;
+    }
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+        return $this;
+    }
+
+    public function getUpdateddAt(): ?\DateTimeInterface 
+    {
+        return $this->updatedAt;
+    }
+    public function setUpdateddAt(\DateTimeInterface $updatedAt): self
+    {
+        $this->updatedAt = $updateddAt;
+        return $this;
+    }
+    /**
+     * @ORM\PrePersist
+     * @ORM\PreUpdate
+     */
+    PUBLIC FUNCTION updateTimestamps()
+    {   
+        if( $this->getCreatedAt()=== null)
+        {
+            $this->setCreatedAt(new \DateTimeImmutable);
+        }
+        $this->setUpdateddAt(new \DateTimeImmutable);
+    }
+    
 }
