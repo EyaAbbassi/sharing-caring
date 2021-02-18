6e66403c3436d06a2e3f4052ff126b10d9fc6c85
ayah1103 <73839197+ayah1103@users.noreply.github.com>
2021-02-18 03:18:31 +0100

    refactoring of create/edit by using PinType and make use of PUT as method for edit

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -8,13 +8,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Common\Persistence\ManagerRegistry;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\Form\FormRegistryInterface;
+use App\Form\PinType;
 
 
 class PinsController extends AbstractController
@@ -35,10 +34,7 @@ class PinsController extends AbstractController
     public function create(Request $request , EntityManagerInterface $em): response
     {
         $pin = new Pin ;
-        $form= $this->createFormBuilder($pin )
-              ->add('title',TextType::class)
-              ->add('description',TextareaType::class)
-              ->getForm();
+        $form= $this->createForm(PinType ::class,$pin );
 
              $form->handleRequest($request);
 
@@ -63,15 +59,12 @@ class PinsController extends AbstractController
  }
 
  /**
-  *  @Route("/pins/{id<[0-9]+>}/edit", name="app_pins_edit",methods="GET|POST")
+  *  @Route("/pins/{id<[0-9]+>}/edit", name="app_pins_edit",methods="GET|Put")
   */
   public function edit(Request $request ,EntityManagerInterface $em,Pin $pin): response
   { 
     
-    $form= $this->createFormBuilder($pin )
-          ->add('title',TextType::class)
-          ->add('description',TextareaType::class)
-          ->getForm();
+    $form= $this->createForm(PinType ::class,$pin, ['method'=>'PUT']);
 
          $form->handleRequest($request);
 
