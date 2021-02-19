3f9cdf75f75b5d0751b316fb6f1572e676b68919
ayah1103 <73839197+ayah1103@users.noreply.github.com>
2021-02-19 18:37:01 +0100

    Add VichupUploaderBundle and edit config

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,6 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+
 ];
