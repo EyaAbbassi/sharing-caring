b7b3e4e05be08a6eeade72471ffed5807e71c264
ayah1103 <73839197+ayah1103@users.noreply.github.com>
2021-02-20 01:28:06 +0100

    add fonction get full name

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -201,6 +201,9 @@ class User implements UserInterface
 
         return $this;
     }
-
+    public function getFullName(): string
+    {
+        return $this->getFirstName() . ' ' . $this->getLastName();
+    }
    
 }
